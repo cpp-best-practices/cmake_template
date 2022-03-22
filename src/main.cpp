6a91aec42e0060ee698c53414f193c72a8d6f2bd
Jason Turner <jason@emptycrate.com>
2022-03-21 20:24:47 -0600

    First take at a bitmap renderer

--- src/main.cpp
+++ src/main.cpp
@@ -98,6 +98,126 @@ template<std::size_t Width, std::size_t Height> struct GameBoard
   }
 };
 
+
+void consequence_game()
+{
+  auto screen = ftxui::ScreenInteractive::TerminalOutput();
+
+  GameBoard<3, 3> gb;
+
+  std::string quit_text;
+
+  const auto update_quit_text = [&quit_text](const auto &game_board) {
+    quit_text = fmt::format("Quit ({} moves)", game_board.move_count);
+    if (game_board.solved()) { quit_text += " Solved!"; }
+  };
+
+  const auto make_buttons = [&] {
+    std::vector<ftxui::Component> buttons;
+    for (std::size_t x = 0; x < gb.width; ++x) {
+      for (std::size_t y = 0; y < gb.height; ++y) {
+        buttons.push_back(ftxui::Button(&gb.get_string(x, y), [=, &gb] {
+          if (!gb.solved()) { gb.press(x, y); }
+          update_quit_text(gb);
+        }));
+      }
+    }
+    return buttons;
+  };
+
+  auto buttons = make_buttons();
+
+  auto quit_button = ftxui::Button(&quit_text, screen.ExitLoopClosure());
+
+  auto make_layout = [&] {
+    std::vector<ftxui::Element> rows;
+
+    std::size_t idx = 0;
+
+    for (std::size_t x = 0; x < gb.width; ++x) {
+      std::vector<ftxui::Element> row;
+      for (std::size_t y = 0; y < gb.height; ++y) {
+        row.push_back(buttons[idx]->Render());
+        ++idx;
+      }
+      rows.push_back(ftxui::hbox(std::move(row)));
+    }
+
+    rows.push_back(ftxui::hbox({ quit_button->Render() }));
+
+    return ftxui::vbox(std::move(rows));
+  };
+
+
+  static constexpr int randomization_iterations = 100;
+  static constexpr int random_seed = 42;
+
+  std::mt19937 gen32{ random_seed };// NOLINT
+  std::uniform_int_distribution<std::size_t> x(static_cast<std::size_t>(0), gb.width - 1);
+  std::uniform_int_distribution<std::size_t> y(static_cast<std::size_t>(0), gb.height - 1);
+
+  for (int i = 0; i < randomization_iterations; ++i) { gb.press(x(gen32), y(gen32)); }
+  gb.move_count = 0;
+  update_quit_text(gb);
+
+  auto all_buttons = buttons;
+  all_buttons.push_back(quit_button);
+  auto container = ftxui::Container::Horizontal(all_buttons);
+
+  auto renderer = ftxui::Renderer(container, make_layout);
+
+  screen.Loop(renderer);
+}
+
+struct Bitmap : ftxui::Node
+{
+  Bitmap(std::size_t width, std::size_t height)  // NOLINT
+    : width_(width), height_(height)
+  {
+  }
+
+  ftxui::Color &at(std::size_t x, std::size_t y) {
+    return pixels.at(width_ * y + x);
+  }
+
+  void ComputeRequirement() override {
+    requirement_ = ftxui::Requirement{.min_x = static_cast<int>(width_), .min_y = static_cast<int>(height_/2), .selected_box{0,0,0,0}};
+  }
+
+  void SetBox(ftxui::Box box) override {
+    box_ = box;
+  }
+
+  void Render(ftxui::Screen &screen) override {
+    auto &p = screen.PixelAt(box_.x_min, box_.y_min);
+    p.character = "▄";
+    p.background_color = ftxui::Color::Red;
+    p.foreground_color = ftxui::Color::Blue;
+  }
+
+  private:
+  std::size_t width_;
+  std::size_t height_;
+
+  std::vector<ftxui::Color> pixels = std::vector<ftxui::Color>(width_ * height_, ftxui::Color::Black);
+
+};
+
+void game_loop_canvas()
+{
+  auto bm = std::make_shared<Bitmap>(50,50); // NOLINT
+
+
+  auto document = bm | ftxui::border;
+
+  auto screen = ftxui::Screen::Create(ftxui::Dimension::Fit(document));
+
+  ftxui::Render(screen, document);
+  screen.Print();
+  getchar();
+
+}
+
 int main(int argc, const char **argv)
 {
   try {
@@ -109,74 +229,9 @@ int main(int argc, const char **argv)
         myproject::cmake::project_version));// version string, acquired
                                             // from config.hpp via CMake
 
-    auto screen = ftxui::ScreenInteractive::TerminalOutput();
+    game_loop_canvas();
 
-    GameBoard<3, 3> gb;
-
-    std::string quit_text;
-
-    const auto update_quit_text = [&quit_text](const auto &game_board) {
-      quit_text = fmt::format("Quit ({} moves)", game_board.move_count);
-      if (game_board.solved()) { quit_text += " Solved!"; }
-    };
-
-    const auto make_buttons = [&] {
-      std::vector<ftxui::Component> buttons;
-      for (std::size_t x = 0; x < gb.width; ++x) {
-        for (std::size_t y = 0; y < gb.height; ++y) {
-          buttons.push_back(ftxui::Button(&gb.get_string(x, y), [=, &gb] {
-            if (!gb.solved()) { gb.press(x, y); }
-            update_quit_text(gb);
-          }));
-        }
-      }
-      return buttons;
-    };
-
-    auto buttons = make_buttons();
-
-    auto quit_button = ftxui::Button(&quit_text, screen.ExitLoopClosure());
-
-    auto make_layout = [&] {
-      std::vector<ftxui::Element> rows;
-
-      std::size_t idx = 0;
-
-      for (std::size_t x = 0; x < gb.width; ++x) {
-        std::vector<ftxui::Element> row;
-        for (std::size_t y = 0; y < gb.height; ++y) {
-          row.push_back(buttons[idx]->Render());
-          ++idx;
-        }
-        rows.push_back(ftxui::hbox(std::move(row)));
-      }
-
-      rows.push_back(ftxui::hbox({ quit_button->Render() }));
-
-      return ftxui::vbox(std::move(rows));
-    };
-
-
-    static constexpr int randomization_iterations = 100;
-    static constexpr int random_seed = 42;
-
-    std::mt19937 gen32{ random_seed };// NOLINT
-    std::uniform_int_distribution<std::size_t> x(static_cast<std::size_t>(0), gb.width - 1);
-    std::uniform_int_distribution<std::size_t> y(static_cast<std::size_t>(0), gb.height - 1);
-
-    for (int i = 0; i < randomization_iterations; ++i) { gb.press(x(gen32), y(gen32)); }
-    gb.move_count = 0;
-    update_quit_text(gb);
-
-    auto all_buttons = buttons;
-    all_buttons.push_back(quit_button);
-    auto container = ftxui::Container::Horizontal(all_buttons);
-
-    auto renderer = ftxui::Renderer(container, make_layout);
-
-    screen.Loop(renderer);
-
-
+//    consequence_game();
   } catch (const std::exception &e) {
     fmt::print("Unhandled exception in main: {}", e.what());
   }
