6d8d80f53118980cacb6568d3609978592c41375
Jason Turner <jason@emptycrate.com>
2023-04-15 18:12:48 -0600

    properly handle not being the top level project

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -1,20 +1,52 @@
-option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ON)
-option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ON)
-option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-option(myproject_ENABLE_CACHE "Enable ccache" ON)
+if(NOT PROJECT_IS_TOP_LEVEL OR PACKAGING_MAINTAINER_MODE)
+  option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
+  option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+  option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+  option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+  option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+  option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+  option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+  option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+  option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+  option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+  option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+  option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
+  option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+else()
+  option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
+  option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+  option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+  option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ON)
+  option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+  option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ON)
+  option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+  option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+  option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+  option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+  option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+  option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
+  option(myproject_ENABLE_CACHE "Enable ccache" ON)
+endif()
 
+if(NOT PROJECT_IS_TOP_LEVEL)
+  mark_as_advanced(
+    myproject_ENABLE_IPO
+    myproject_WARNINGS_AS_ERRORS
+    myproject_ENABLE_USER_LINKER
+    myproject_ENABLE_SANITIZER_ADDRESS
+    myproject_ENABLE_SANITIZER_LEAK
+    myproject_ENABLE_SANITIZER_UNDEFINED
+    myproject_ENABLE_SANITIZER_THREAD
+    myproject_ENABLE_SANITIZER_MEMORY
+    myproject_ENABLE_UNITY_BUILD
+    myproject_ENABLE_CLANG_TIDY
+    myproject_ENABLE_CPPCHECK
+    myproject_ENABLE_PCH
+    myproject_ENABLE_CACHE)
+endif()
 
-include(cmake/InterproceduralOptimization.cmake)
 if(myproject_ENABLE_IPO)
+  include(cmake/InterproceduralOptimization.cmake)
   enable_ipo()
 endif()
 
@@ -29,13 +61,13 @@ include(cmake/CompilerWarnings.cmake)
 set_project_warnings(
   myproject_warnings
   ${myproject_WARNINGS_AS_ERRORS}
-  "" 
+  ""
   ""
   ""
   "")
 
-include(cmake/Linker.cmake)
 if(myproject_ENABLE_USER_LINKER)
+  include(cmake/Linker.cmake)
   configure_linker(myproject_options)
 endif()
 
@@ -50,7 +82,6 @@ enable_sanitizers(
 
 set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
 
-
 if(myproject_ENABLE_PCH)
   target_precompile_headers(
     myproject_options
@@ -60,7 +91,7 @@ if(myproject_ENABLE_PCH)
     <utility>)
 endif()
 
-if (myproject_ENABLE_CACHE)
+if(myproject_ENABLE_CACHE)
   include(cmake/Cache.cmake)
   enable_cache()
 endif()
@@ -71,9 +102,6 @@ if(myproject_ENABLE_CLANG_TIDY)
 endif()
 
 if(myproject_ENABLE_CPPCHECK)
-  enable_cppcheck(${myproject_WARNINGS_AS_ERRORS}
-    "" # override cppcheck options
-    )
+  enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  )
 endif()
-
-
