714168c4dc854912625b9236ed01134460c465bd
Jason Turner <jason@emptycrate.com>
2023-04-16 18:02:16 -0600

    Try to limit

--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -7,8 +7,14 @@ macro(enable_hardening target)
     target_compile_options(${target} INTERFACE -include "${CMAKE_SOURCE_DIR}/cmake/_FORTIFY_SOURCE.hpp")
     target_compile_options(${target} INTERFACE -fstack-protector-strong -fcf-protection)
     if (NOT WIN32)
-      target_link_options(${target} INTERFACE -pie -fstack-clash-protection)
+      target_link_options(${target} INTERFACE -pie)
+
+      if (NOT (APPLE AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+        # only enable if on Linux or on GCC
+        target_link_options(${target} INTERFACE -fstack-clash-protection)
+      endif()
     endif()
+
   endif()
 
 
