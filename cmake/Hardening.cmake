fdcc74a86569cd33f6014c69d2d7c8a804b5d120
Jason Turner <jason@emptycrate.com>
2023-04-16 18:07:52 -0600

    Attempt to simplify hardening flag detection

--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -5,15 +5,28 @@ macro(enable_hardening target)
   elseif(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang|GNU")
     target_compile_definitions(${target} INTERFACE _GLIBCXX_ASSERTIONS)
     target_compile_options(${target} INTERFACE -include "${CMAKE_SOURCE_DIR}/cmake/_FORTIFY_SOURCE.hpp")
-    target_compile_options(${target} INTERFACE -fstack-protector-strong -fcf-protection)
-    if (NOT WIN32)
-      target_link_options(${target} INTERFACE -pie)
 
-      if (NOT (APPLE AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-        # only enable if on Linux or on GCC
-        target_link_options(${target} INTERFACE -fstack-clash-protection)
-      endif()
+    check_cxx_compiler_flag(-fpie PIE)
+    if (PIE)
+      target_compile_options(${target} INTERFACE -fpie)
+      target_link_options(${target} INTERFACE -pie)
     endif()
+
+    check_cxx_compiler_flag(-fstack-protector-strong STACK_PROTECTOR)
+    if (STACK_PROTECTOR)
+      target_compile_options(${target} INTERFACE -fstack-protector-strong)
+    endif()
+
+    check_cxx_compiler_flag(-fcf-protection CF_PROTECTION)
+    if (CF_PROTECTION)
+      target_compile_options(${target} INTERFACE -fcf-protection)
+    endif()
+
+    check_cxx_compiler_flag(-fclash-protection CLASH_PROTECTION)
+    if (CLASH_PROTECTION)
+      target_compile_options(${target} INTERFACE -fstack-clash-protection)
+    endif()
+
 
   endif()
 
