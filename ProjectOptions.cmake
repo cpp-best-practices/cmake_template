58d32ed761d5b8867adf9cf039ef120babe1e54f
Jason Turner <jason@emptycrate.com>
2025-04-23 09:06:25 -0600

    Sanity check UBSan as well

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -9,7 +9,22 @@ include(CheckCXXSourceCompiles)
 
 macro(myproject_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
-    set(SUPPORTS_UBSAN ON)
+
+    message(STATUS "Sanity checking UndefinedBehaviorSanitizer, it should be supported on this platform")
+    set(TEST_PROGRAM "int main() { return 0; }")
+
+    # Check if UndefinedBehaviorSanitizer works at link time
+    set(CMAKE_REQUIRED_FLAGS "-fsanitize=undefined")
+    set(CMAKE_REQUIRED_LINK_OPTIONS "-fsanitize=undefined")
+    check_cxx_source_compiles("${TEST_PROGRAM}" HAS_UBSAN_LINK_SUPPORT)
+
+    if(HAS_UBSAN_LINK_SUPPORT)
+      message(STATUS "UndefinedBehaviorSanitizer is supported at both compile and link time.")
+      set(SUPPORTS_UBSAN ON)
+    else()
+      message(WARNING "UndefinedBehaviorSanitizer is NOT supported at link time.")
+      set(SUPPORTS_UBSAN OFF)
+    endif()
   else()
     set(SUPPORTS_UBSAN OFF)
   endif()
