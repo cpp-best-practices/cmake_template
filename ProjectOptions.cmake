eb558d77899971f02d2807b621b3f9b5dd0744a3
Jason Turner <jason@emptycrate.com>
2025-04-23 08:55:00 -0600

    Update for broken ASAN on gcc on MacOS

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,6 +4,9 @@ include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
 
 
+include(CheckCXXSourceCompiles)
+
+
 macro(myproject_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
     set(SUPPORTS_UBSAN ON)
@@ -14,7 +17,25 @@ macro(myproject_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND WIN32)
     set(SUPPORTS_ASAN OFF)
   else()
-    set(SUPPORTS_ASAN ON)
+    if (NOT WIN32)
+      message(STATUS "Sanity checking AddressSanitizer, it should be supported on this platform")
+      set(TEST_PROGRAM "int main() { return 0; }")
+
+      # Check if AddressSanitizer works at link time
+      set(CMAKE_REQUIRED_FLAGS "-fsanitize=address")
+      set(CMAKE_REQUIRED_LINK_OPTIONS "-fsanitize=address")
+      check_cxx_source_compiles("${TEST_PROGRAM}" HAS_ASAN_LINK_SUPPORT)
+
+      if(HAS_ASAN_LINK_SUPPORT)
+        message(STATUS "AddressSanitizer is supported at both compile and link time.")
+        set(SUPPORTS_ASAN ON)
+      else()
+        message(WARNING "AddressSanitizer is NOT supported at link time.")
+        set(SUPPORTS_ASAN OFF)
+      endif()
+    else()
+      set(SUPPORTS_ASAN ON)
+    endif()
   endif()
 endmacro()
 
